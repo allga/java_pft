70ebeef6101d3130ad1c9739d1d119aeff3a167d
Olga Penner <olga.penner@together.com>
2016-02-29 22:05:32 +0200

    class ApplicationManager created & methods from TestBase are moved

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -6,9 +6,9 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    gotoAddNewPage();
-    fillContactForm(new ContactData("Ivan", "Ivanov", "Noosphere", "Shevchenko, 59", "56-373-22-89", "50-362-85-96"));
-    submitContact();
+    app.gotoAddNewPage();
+    app.fillContactForm(new ContactData("Ivan", "Ivanov", "Noosphere", "Shevchenko, 59", "56-373-22-89", "50-362-85-96"));
+    app.submitContact();
   }
 
 }
