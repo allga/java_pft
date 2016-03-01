4c1393ecc43b74f29fa77142cc9713f76e6125af
Olga Penner <olga.penner@together.com>
2016-03-01 10:28:09 +0200

    class HelperBase with methods click & type is created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,29 +7,22 @@ import ru.stqa.pft.addressbook.model.ContactData;
 /**
  * Created by Olga on 29.02.2016.
  */
-public class ContactHelper {
-    private FirefoxDriver wd;
+public class ContactHelper extends HelperBase {
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submitContact() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillContactForm(ContactData contact) {
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contact.getFirstname());
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contact.getLastname());
-      wd.findElement(By.name("company")).clear();
-      wd.findElement(By.name("company")).sendKeys(contact.getCompany());
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contact.getAddress());
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contact.getHomephone());
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contact.getMobilephone());
+        type(By.name("firstname"), contact.getFirstname());
+        type(By.name("lastname"), contact.getLastname());
+        type(By.name("company"), contact.getCompany());
+        type(By.name("address"), contact.getAddress());
+        type(By.name("home"), contact.getHomephone());
+        type(By.name("mobile"), contact.getMobilephone());
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Olga on 29.02.2016.
  */
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase{
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
+
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void gotoAddNewPage() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
     }
 }
