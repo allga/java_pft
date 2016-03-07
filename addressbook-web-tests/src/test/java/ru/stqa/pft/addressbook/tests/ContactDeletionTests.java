637ea3447be87e68bd8eb020ef4c9456be87d816
Olga Penner <olga.penner@together.com>
2016-03-07 13:14:24 +0200

    Revert "Revert "try to do method createContact""
    This reverts commit 3f5f7e16fea2666e470a8d06fbb5119d405eb2e4.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
  * Created by Olga on 01.03.2016.
@@ -10,6 +11,9 @@ public class ContactDeletionTests extends TestBase {
     @Test
     public void testContactDeletion() {
         app.getNavigationHelper().gotoHome();
+//        if (! app.getContactHelper().isThereAContact()) {
+//            app.getContactHelper().createContact(new ContactData("Ivan", "Ivanov", "Noosphere", "Shevchenko, 59", "56-373-22-89", "50-362-85-96", "test1"), true);
+//        }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContact();
         app.getContactHelper().submitContactDeletion();
