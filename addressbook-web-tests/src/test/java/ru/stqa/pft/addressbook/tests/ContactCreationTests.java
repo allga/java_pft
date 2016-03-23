4a9c95b0f651afd350a4551b8386518debf870f7
Olga Borovyk <pennerall@gmail.com>
2016-03-23 22:52:15 +0200

    ContactCreationTests modified using class Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
   @Test (enabled = true)
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoHome();
-    Set<ContactData> before = app.getContactHelper().getAllContacts();
+    Contacts before = app.getContactHelper().getAllContacts();
     ContactData contact = new ContactData().
             setFirstname("Leonid").setLastname("Ivanov").setCompany("Noosphere").setAddress("Shevchenko, 59").
             setHomephone("56-373-22-89").setMobilephone("50-362-85-96").setGroup("test1");
     app.getContactHelper().createContact(contact, true);
     Set<ContactData> after = app.getContactHelper().getAllContacts();
-    Assert.assertEquals(after.size(), before.size() + 1);
+    assertThat(after.size(), equalTo(before.size() + 1));
 
-    contact.setId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    assertThat(after, equalTo(
+            before.withAdded(contact.setId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
 
