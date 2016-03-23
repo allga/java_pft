de0ac6080be53a2ef833a2a437f60e129ca2d320
Olga Borovyk <pennerall@gmail.com>
2016-03-23 21:51:48 +0200

    GroupModificationTests modified using class Groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,8 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Olga on 01.03.2016.
@@ -22,16 +24,14 @@ public class GroupModificationTests extends TestBase {
 
     @Test (enabled = true)
     public void testGroupModification() {
-        Set<GroupData> before = app.getGroupHelper().getAllGroups();
+        Groups before = app.getGroupHelper().getAllGroups();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData().setId(modifiedGroup.getId()).setName("test1111");
         app.getGroupHelper().modifyGroup(group);
-        Set<GroupData> after = app.getGroupHelper().getAllGroups();
-        Assert.assertEquals(after.size(), before.size());
+        Groups after = app.getGroupHelper().getAllGroups();
+        assertThat(after.size(), equalTo(before.size()));
 
-        before.remove(modifiedGroup);
-        before.add(group);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
 }
