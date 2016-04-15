7f7f8d25073afe925ac4ea0d7d58ef382672fd7a
Olga Borovyk <pennerall@gmail.com>
2016-04-15 15:02:37 +0300

    DbHelper created

---
+++ mantis-tests/src/test/java/ru/stqa/ptf/mantis/appmanager/DbHelper.java
@@ -0,0 +1,36 @@
+package ru.stqa.ptf.mantis.appmanager;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.stqa.ptf.mantis.model.User;
+import ru.stqa.ptf.mantis.model.Users;
+
+import java.util.List;
+
+/**
+ * Created by Olga on 10.04.2016.
+ */
+public class DbHelper {
+
+    private final SessionFactory sessionFactory;
+
+    public DbHelper() {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+
+    public Users getUsersFromBD() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<User> result = session.createQuery( "from mantis_user_table" ).list();
+        session.getTransaction().commit();
+        session.close();
+        return new Users(result);
+    }
+}
