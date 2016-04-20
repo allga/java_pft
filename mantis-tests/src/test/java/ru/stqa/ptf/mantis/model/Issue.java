7f602da9a66bde273dd4073d586f2867a6a28ae5
Olga Borovyk <pennerall@gmail.com>
2016-04-20 17:43:28 +0300

    model of Issue created

---
+++ mantis-tests/src/test/java/ru/stqa/ptf/mantis/model/Issue.java
@@ -0,0 +1,48 @@
+package ru.stqa.ptf.mantis.model;
+
+/**
+ * Created by Olga on 20.04.2016.
+ */
+public class Issue {
+
+    private int id;
+    private String summary;
+    private String description;
+    private Project project;
+
+    public int getId() {
+        return id;
+    }
+
+    public Issue setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public Issue setSummary(String summary) {
+        this.summary = summary;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Issue setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public Issue setProject(Project project) {
+        this.project = project;
+        return this;
+    }
+}
